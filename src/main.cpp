a31ed1f489b5e56d7339b0f713315b64c0c29de1
lucaslin989-cloud <lucaslin989@gmail.com>
2026-01-20 17:40:49 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+Make a program where you enter the height of a pyramid
+and the program will print out that pyramid.
+
+For example, if I was to type 5, it would show
+     *
+    * *
+   * * *
+  * * * *
+* * * * * *
+
+If I was to type 3 it would show
+   *
+  * *
+* * * *
+
+*/
