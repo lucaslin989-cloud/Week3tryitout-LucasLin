e3a4498646eb41a93c5bd3a8466c5d0af0a2c8e7
Lucas Lin <lucaslin989@gamil.com>
2026-01-20 19:24:47 -0700

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -15,3 +15,40 @@ If I was to type 3 it would show
 * * * *
 
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+
+  cout<<"Type a int number: ";
+  int num=0;
+  cin>> num;
+  for(int row=1;row<=num;row++){
+    for (int space=1;space<=num-row;space++){
+      cout<<" ";
+    }
+    
+    for(int star=1; star<=row;star++){
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+  cout<<endl;
+
+
+
+  for(int row=1;row<=num;row++){
+    for (int space=1;space<=num-row;space++){
+      cout<<" ";
+    }
+    for(int star=1; star<=(row*2-1);star++){
+      cout<<"*";
+    }
+    cout<<endl;
+
+  }
+
+  return 0;
+}
